3704b5bcb74a9f0c0e0ba45d309019f3e306eb50
Mark Ito <marki@jlab.org>
2020-05-28 20:48:49 -0400

    intermediate

--- bin/extract_parameter.sh
+++ bin/extract_parameter.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 config_file=$1
 parameter_name=$2
-grep $parameter_name $config_file | awk -F= '{print $2}'
+parameter_default_value=$3
+if grep -q $parameter_name $config_file
+    then
+    grep $parameter_name $config_file | awk -F= '{print $2}'
+else
+    if [ ! -z "$parameter_default_value" ]
+	then
+	echo $parameter_default_value
+    else
+	echo "no value found for parameter $parameter_name"
+	exit 1
+    fi
+fi
