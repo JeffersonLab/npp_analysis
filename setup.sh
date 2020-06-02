69410e7c219831169f0924ea108444c7f0ed2d2e
Mark Ito <marki@jlab.org>
2020-06-02 14:48:13 -0400

    separate setup into two files, one for bash one for tcsh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+. /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
+gxenv $HALLD_VERSIONS/npp_1.0.xml
+export JANA_CALIB_CONTEXT="variation=mc_cpp"   
+export JANA_GEOMETRY_URL="ccdb:///GEOMETRY/cpp_HDDS.xml"
