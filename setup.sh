a0afa9dc9b81d8f40a4138e2a49b4421862e866f
Mark Ito <marki@jlab.org>
2020-07-10 12:59:30 -0400

    changes for bggen

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 . /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/npp_1.0.xml
+gxenv $HALLD_VERSIONS/npp_1.1.xml
 export JANA_CALIB_CONTEXT="variation=mc_cpp"   
 export JANA_GEOMETRY_URL="ccdb:///GEOMETRY/cpp_HDDS.xml"
